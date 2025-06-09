2f07d17108557fac90a07c307bfbec8459554881
murilobf <147562088+murilobf@users.noreply.github.com>
2025-06-09 09:45:44 -0300

    Add files via upload

--- teste5sensores.ino
+++ teste5sensores.ino
@@ -1,73 +1,55 @@
 #include <AFMotor.h>
+
 /// Porta Sensor de Linha Preta Esquerdo mais distante do centro
-#define PSLP_ESQ_EX 7
+#define PSLP_ESQ_EX 42
 // Porta Sensor de Linha Preta Esquerda mais perto do centro 
-#define PSLP_ESQ_IN 8
+#define PSLP_ESQ_IN 44
 // Porta Sensor de Linha Preta do centro
-#define PSLP_MEIO 9
+#define PSLP_MEIO 46
 // Porta Sensor de Linha Preta Direito mais perto do centro
-#define PSLP_DIR_IN 10
+#define PSLP_DIR_IN 48
 // Porta Sensor de Linha Preta Direito mais distante do centro
-#define PSLP_DIR_EX 11
+#define PSLP_DIR_EX 50
 
 
-// Porta do motor esquerdo
-#define PORTA_MOT_E 1
-// Porta do motor direito
-#define PORTA_MOT_D 2
+// Portas do motor esquerdo
+AF_DCMotor motorE(1);
+AF_DCMotor motorD(2);
+
+uint8_t velocidade_rap = 200;
+uint8_t velocidade_med = 180;
+uint8_t velocidade_len = 150;
 
 // globais para armazenar a leitura dos sensores de linha
+// OBS: para o sensor de 5 linhas preto é 0 e branco é 1
 int sen_esq_ext = 0;
 int sen_esq_int = 0;
 int sen_meio = 0;
 int sen_dir_int = 0;
 int sen_dir_ext = 0;
 
-typedef struct Vetor2i{
-    int x;
-    int y;
-} Vetor2i;
-
-Vetor2i rotacao;
-
-#define MOV_LEVE 1
-#define MOV_BRUCO 2
-#define MOV_DELAY 3.0
-
-AF_DCMotor Motor_esq(PORTA_MOT_E);
-AF_DCMotor Motor_dir(PORTA_MOT_D);
-
-uint8_t velocidade = 200;
-
-#define INC_MOTOR_REAL 0.02 // 2%
-double porcentagem_motor_Esq_real = 0.9;
-double porcentagem_motor_Dir_real = 0.9;
 
 void setup()
 {
     // inicializando os sensores de linha preta
+
+    Serial.begin(9600);
+
     pinMode(PSLP_ESQ_EX, INPUT);
     pinMode(PSLP_ESQ_IN, INPUT);
     pinMode(PSLP_MEIO, INPUT);
     pinMode(PSLP_DIR_IN, INPUT);
     pinMode(PSLP_DIR_EX, INPUT);
 
-    Serial.begin(9600);
+    motorE.setSpeed(velocidade_med);
+    motorD.setSpeed(velocidade_med);
 }
 
 // loop de estados simples para gerar rotina
 void loop()
 {
     le_linhas();
-    
-    Serial.println(sen_esq_ext);
-    Serial.println(sen_esq_int);
-    Serial.println(sen_meio);
-    Serial.println(sen_dir_int);
-    Serial.println(sen_dir_ext);
-
     define_curva();
-    anda();
     delay(1000);
 }
 
@@ -76,122 +58,121 @@ void le_linhas()
 {
      sen_esq_ext = digitalRead(PSLP_ESQ_EX);
      sen_esq_int = digitalRead(PSLP_ESQ_IN);
-     sen_meio = digitalRead(PSLP_MIN);
+     sen_meio = digitalRead(PSLP_MEIO);
      sen_dir_int = digitalRead(PSLP_DIR_IN);
      sen_dir_ext = digitalRead(PSLP_DIR_EX);
+
+     /*Serial.print(sen_esq_ext);
+     Serial.print(sen_esq_int);
+     Serial.print(sen_meio);
+     Serial.print(sen_dir_int);
+     Serial.print(sen_dir_ext);
+     Serial.println();*/
 }
 
 // função que avalia as informações lidas e escolhe que caminho seguir
+// OBS: para o sensor de 5 linhas preto é 0 e branco é 1
 void define_curva()
 {
-    // possibilidades de leitura
-    // definir o comportamento pra mais de uma leitura igual e nao só na primeira
+    // Casos curva brusca a esquerda
+    // O valor do meio não importa pois, tanto para 0 quanto pra 1, se os dois sensores da esquerda estão ativados ele precisa virar pra esquerda
+    if(sen_esq_ext == 0 &&
+       sen_esq_int == 0 && 
+       sen_dir_int == 1 &&
+       sen_dir_ext == 1){
 
-    //arvore de escolhas
-    if (sen_Esq_Meio)
-    {
-        if (sen_Dir_Meio)
-        {
-            if (sen_Esq_Lado)
-            {
-                if (sen_Dir_Lado)
-                    para(); // P P P P
-                else // !sen_Dir_Lado
-                    curva_quadrada_esq(); // P P P B
-            }
-            else // !sen_Esq_Lado
-            {
-                if (sen_Dir_Lado)
-                    curva_quadrada_dir(); // B P P P
-                else // !sen_Dir_Lado
-                    reto(); // B P P B
-            }
-        }
-        else // !sen_Dir_Meio
-        {
-            if ( !(sen_Esq_Lado || sen_Dir_Lado) )
-                curva_pouquinho_dir(); // B P B B
-        }
-    }
-    else // !sen_Esq_Meio
-    {
-        if(sen_Dir_Meio)
-            if(!(sen_Esq_Lado || sen_Dir_Lado))
-                curva_pouquinho_esq(); // B B P B
-        else // !sen_Dir_Meio
-        {
-            if( !(sen_Esq_Lado || sen_Dir_Lado) )
-                anda_sem_rumo(); // B B B B
-            else if (sen_Esq_Lado)
-                curva_quadrada_esq(); // P B B B
-            else if (sen_Dir_Lado)
-                curva_quadrada_dir(); // B B B P
-        }
-    }
-}
+          forte_esquerda();
+          Serial.println("fore\n");
+       }
 
-// funcoes de definicao de rotacao // TEM MUITO O QUE MELHORAR, È SÒ UMA BASE
+    // Caso curva mais suave a esquerda
+    // Idem para meio
+    else if((sen_esq_ext == 1 &&
+       sen_esq_int == 0) || 
+       (sen_esq_ext == 0 &&
+       sen_esq_int == 1)&&
+       sen_dir_int == 1 &&
+       sen_dir_ext == 1){
 
-void para()
-{
-    rotacao.x = 0;
-    rotacao.y = 0;
-    set_motores(0, RELEASE, RELEASE);
-}
-void reto()
-{
-    rotacao.x = 0;
-    rotacao.y = 0;
-}
-void curva_quadrada_esq()
-{
-    rotacao.x = 0;
-    rotacao.y = 90;
-}
-void curva_quadrada_dir()
-{
-    rotacao.x = 0;
-    rotacao.y = -90;
-}
-void curva_pouquinho_esq()
-{
-    rotacao.x = 0;
-    rotacao.y = 30;
-}
-void curva_pouquinho_dir()
-{
-    rotacao.x = 0;
-    rotacao.y = -30;
-}
-void anda_sem_rumo()
-{
-    reto();
+        fraco_esquerda();
+        Serial.println("frae\n");
+       }
+
+    // Vai reto, como os valores dos sensores da esquerda já foram verificados, eles não são necessários, mantenho apenas por questão de legibilidade
+    else if(sen_esq_ext == 1 &&
+       sen_esq_int == 1 && 
+       sen_meio == 0 &&
+       sen_dir_int == 1 &&
+       sen_dir_ext == 1){
+
+        anda_frente();
+        Serial.println("frent\n");
+       }
+
+    // Curva suave a direita
+    // Idem para o meio
+    else if(sen_esq_ext == 1 &&
+       sen_esq_int == 1 && 
+       (sen_dir_int == 0 &&
+       sen_dir_ext == 1) ||
+       (sen_dir_int == 1 &&
+       sen_dir_ext == 0)){
+
+        fraco_direita();
+        Serial.println("fradir\n");
+       }
+
+    // Curva brusca a direita
+    // Idem para o meio
+    else if(sen_esq_ext == 1 &&
+       sen_esq_int == 1 && 
+       sen_dir_int == 0 &&
+       sen_dir_ext == 0){
+
+        forte_direita();
+        Serial.println("fordir\n");
+       }
 }
 
-// dada a escolha de caminho essa função segue o caminho
-void anda()
-{
-   /* if (rotacao.y > 0)
-        curva(ESQUERDA, rotacao.y);
-    else if (rotacao.y < 0)
-        curva(DIREITA, abs(rotacao.y));*/
+void forte_esquerda(){
+    motorE.setSpeed(0);
+    motorD.setSpeed(velocidade_med);
+
+    motorE.run(FORWARD);
+    motorD.run(FORWARD);
 }
 
+void fraco_esquerda(){
 
-void set_motores(uint8_t velocidade, int direcao_esq, int direcao_dir)
-{
-  Motor_esq.setSpeed((uint8_t) (velocidade * porcentagem_motor_Esq_real));
-  Motor_dir.setSpeed((uint8_t) (velocidade * porcentagem_motor_Dir_real));
-  Motor_esq.run(direcao_esq);
-  Motor_dir.run(direcao_dir);
+    motorE.setSpeed(velocidade_len);
+    motorD.setSpeed(velocidade_med);
+
+    motorE.run(FORWARD);
+    motorD.run(FORWARD);
 }
 
-void curva(int direcao, double angulo)
-{  
-  /*if (direcao == ESQUERDA)
-    set_motores(velocidade, FORWARD, BACKWARD);
-  else if (direcao == DIREITA)
-    set_motores(velocidade, BACKWARD, FORWARD);
+void anda_frente(){
+    motorE.setSpeed(velocidade_med);
+    motorD.setSpeed(velocidade_med);
 
-  delay(MOV_DELAY * angulo);*/
+    motorE.run(FORWARD);
+    motorD.run(FORWARD);
+}
+
+void fraco_direita(){
+
+    motorE.setSpeed(velocidade_med);
+    motorD.setSpeed(velocidade_len);
+
+    motorE.run(FORWARD);
+    motorD.run(FORWARD);
+}
+
+void forte_direita(){
+
+    motorE.setSpeed(velocidade_med);
+    motorD.setSpeed(0);
+
+    motorE.run(FORWARD);
+    motorD.run(FORWARD);
 }
